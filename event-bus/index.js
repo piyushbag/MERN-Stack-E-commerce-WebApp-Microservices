58d2008c717ba6c14acabce054fe849aeb69f1ef
Piyush Bag <piyushbag4@gmail.com>
2021-07-26 17:02:49 -0700

    32. Emitting Events

--- event-bus/index.js
+++ event-bus/index.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(bodyParser.json());             
 
 app.post('/events', (req, res) => {
     const event = req.body();
@@ -12,7 +12,7 @@ app.post('/events', (req, res) => {
     axios.post('http://localhost:4000/events', event);
     axios.post('http://localhost:4001/events', event);
     axios.post('http://localhost:4002/events', event);
-
+    
     res.send({ status: 'OK' });
 });
 
